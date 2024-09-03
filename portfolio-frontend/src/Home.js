574b49c6588cdceca01b0887c61af85efc16473c
md reaz morshed <mdreazmorshed@mds-MacBook-Pro.local>
2024-09-03 12:01:32 +0200

    tailwind added

---
+++ portfolio-frontend/src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+    
+    
+    <div>
+             hhhhh
+    </div>
+            
+        
+        
+        
+        
+        );
+}
+ 
+export default Home;
